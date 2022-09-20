00ec57d08554e36e8b2453a9a50a684cc079ffa6
Jummy007 <gbemisoyeadejumoke@gmail.com>
2022-09-20 02:49:34 +0100

    hamburger for smaller devices

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,26 @@
 import React from "react";
 import "./Navbar.css";
 import { images } from "../../constants";
+import {GrMenu} from "react-icons/gr";
 
 const Navbar = () => {
   return (
-    <nav>
-      <div>
-        <img src={images.logo} alt="Logo" />
-        <h2>SOUNDWAVE</h2>
-      </div>
-      <div className="navLinks">
-        <a href="">Discover</a>
-        <a href="">Join</a>
-      </div>
-    </nav>
+    <header>
+      <nav>
+        <div>
+          <img src={images.logo} alt="Logo" />
+          <h2>SOUNDWAVE</h2>
+        </div>
+        <div className="navLinks">
+          <a href="">Discover</a>
+          <a href="">Join</a>
+        </div>
+        <div className="menu">
+          <GrMenu />
+        </div>
+        {/* 768 */}
+      </nav>
+    </header>
   );
 };
 
