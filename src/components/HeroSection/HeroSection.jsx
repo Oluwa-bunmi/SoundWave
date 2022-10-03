17f5529a8cf755329170576d4f529d2d41bba442
Jummy007 <gbemisoyeadejumoke@gmail.com>
2022-10-03 02:37:20 +0100

    herosection in progress

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -13,13 +13,17 @@ const HeroSection = () => {
             <img src={images.girl} alt="" />
           </div>
           <div className="flex flex-col space-y-12 md:w-1/2">
-            <h1 className="max-w-md text-center text-4xl md:text-left md:text-5xl">
+            <h1 className="max-w-md text-white text-center text-4xl md:text-left md:text-5xl">
               Feel The Music
             </h1>
-            <p className="max-w-sm text-center md:text-left">
+            <p className="max-w-sm text-white text-center md:text-left">
               Stream over 20 thousand songs with one click
             </p>
-            <Link to="/join"></Link>
+            <div className="flex justify-center md:justify-start">
+              <Link to="/join" className="text-white bg-pryblue p-3 pt-2">
+                JOIN NOW
+              </Link>
+            </div>
           </div>
         </div>
       </section>
